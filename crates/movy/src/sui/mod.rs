4fbb3878398ec855b3567593eda7f6f551195018
lazymio <mio@lazym.io>
2026-02-25 14:31:13 +0000

    various hack to publish big packages

--- crates/movy/src/sui/mod.rs
+++ crates/movy/src/sui/mod.rs
@@ -31,6 +31,28 @@ pub struct SuiArgs {
 
 impl SuiArgs {
     pub async fn run(self) -> Result<(), MovyError> {
+        // Increase the maximum move package size
+        // Super safe because we are the only active thread at this moment
+        unsafe {
+            std::env::set_var("SUI_PROTOCOL_CONFIG_OVERRIDE_ENABLE", "1");
+            std::env::set_var(
+                "SUI_PROTOCOL_CONFIG_OVERRIDE_MAX_MOVE_PACKAGE_SIZE",
+                "16777216",
+            );
+            std::env::set_var("SUI_PROTOCOL_CONFIG_OVERRIDE_BASE_TX_COST_PER_BYTE", "0");
+            std::env::set_var(
+                "SUI_PROTOCOL_CONFIG_OVERRIDE_OBJ_ACCESS_COST_MUTATE_PER_BYTE",
+                "0",
+            );
+            std::env::set_var(
+                "SUI_PROTOCOL_CONFIG_OVERRIDE_OBJ_ACCESS_COST_VERIFY_PER_BYTE",
+                "0",
+            );
+            std::env::set_var(
+                "SUI_PROTOCOL_CONFIG_OVERRIDE_PACKAGE_PUBLISH_COST_PER_BYTE",
+                "0",
+            );
+        }
         match self.cmd {
             SuiSubcommand::TraceTx(args) => args.run().await?,
             SuiSubcommand::Fuzz(args) => args.run().await?,
